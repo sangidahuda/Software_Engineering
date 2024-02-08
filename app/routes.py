d516ac780151e5eb5d7d4f4b07a07cc71bfaecdc
Jorge Gonzales <95510250+JorgeG2@users.noreply.github.com>
2024-02-07 22:41:16 -0500

    added comment message to the route file

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+"""
+routes.py
+
+This file is the traffic controller of our application. It listens for requests from users 
+(like clicking a link or submitting a form) and then tells the application how to respond. 
+For each route (URL path), we define a function that creates the page the user sees. 
+These functions can pull information from our database, use it to fill out templates, or accept 
+user input to create new records in our database.
+"""
