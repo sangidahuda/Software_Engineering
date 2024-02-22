bcb13bce38ab95e4125340112235fa48e298d39a
Jorge Gonzales <95510250+JorgeG2@users.noreply.github.com>
2024-02-22 11:51:42 -0500

    addedd another form

--- app/forms.py
+++ app/forms.py
@@ -20,7 +20,13 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('Email already in use. Please choose a different one.')
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user:
+    #         raise ValidationError('Email already in use. Please choose a different one.')
+        
+    # here we are checking if the username is already in use
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
