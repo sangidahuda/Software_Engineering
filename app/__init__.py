44242eb3ee2a5604cd4e49ca1ec8c5dd46c13204
Jorge Gonzales <95510250+JorgeG2@users.noreply.github.com>
2024-03-26 16:30:03 -0400

    worked on admin dashboard styling

--- app/__init__.py
+++ app/__init__.py
@@ -4,28 +4,36 @@ from .extensions import db, login_manager
 from flask_migrate import Migrate
 
 
+
+
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = '874y57843hfek43rsd4r4'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:beedrill!@localhost/GSG'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    migrate = Migrate(app, db)
+   app = Flask(__name__)
+   app.config['SECRET_KEY'] = '874y57843hfek43rsd4r4'
+   app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Manchester10!@localhost/GSG'
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   migrate = Migrate(app, db)
 
-    app.config['UPLOAD_FOLDER'] = 'C:\Users\dbopp\Documents\Clone\WebsiteImages'    
 
-    db.init_app(app)
-    login_manager.init_app(app)
-    from .models import User  
+   app.config['UPLOAD_FOLDER'] = '/Users/jorgegonzales/Desktop/WebsiteImages'   
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
-    from .routes import main_bp
-    app.register_blueprint(main_bp)
+   db.init_app(app)
+   login_manager.init_app(app)
+   from .models import User 
+
+
+   @login_manager.user_loader
+   def load_user(user_id):
+       return User.query.get(int(user_id))
+
+
+   from .routes import main_bp
+   app.register_blueprint(main_bp)
+
 
 # Create database tables for our data models
-    with app.app_context():
-        db.create_all()  
+   with app.app_context():
+       db.create_all() 
 
-    return app
+
+   return app
